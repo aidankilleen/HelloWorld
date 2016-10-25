86138e6853c59c3d1e2597ab75256bec58e6bccf
Aidan Killeen <aidan.killeen@gmail.com>
2016-10-25 23:38:57 +0100

    'test'

---
+++ src/main/java/ie/rccourse/webapp/RcHelloWorld/App.java
@@ -0,0 +1,13 @@
+package ie.rccourse.webapp.RcHelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
